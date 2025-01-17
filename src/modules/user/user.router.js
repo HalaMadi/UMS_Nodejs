3a8800cf0723f07d3ea267db36f2dba62f5e4cef
HalaMadi <Halamadi75@gmail.com>
2025-01-17 09:57:21 +0200

    get user

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -5,12 +5,30 @@ import UserModel from "../../../DB/Model/user.model.js";
 
 const router = Router();
 
-// /* Register Endpoint */
 // Get Users 
 router.get('/users', async (req, res) => {
-    const users = await UserModel.findAll();
+    const users = await UserModel.findAll({
+        attributes:['userName','email']
+    });
     return res.status(200).json({ message: "Success", users })
 })
+// delete User 
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const user = await UserModel.findOne(id)
+
+    if (!user) {
+        return res.status(404).json({ message: 'Invalid' })
+    }
+    await UserModel.destroy({
+        where: {
+            id
+        },
+    })
+    return res.status(200).json({ message: "User deleted Successfully" })
+})
+// /* Register Endpoint */
 
 // Add user
 router.post('/register', async (req, res) => {
@@ -30,10 +48,10 @@ router.post('/login', async (req, res) => {
         return res.status(404).json({ message: 'Invalid email' })
     }
     const checkPassword = await bcrypt.compareSync(password, user.password);
-    if(checkPassword==false){
-        return res.status(400).json({message:'Invalid Password'})
+    if (checkPassword == false) {
+        return res.status(400).json({ message: 'Invalid Password' })
     }
-    const token=jwt.sign({id:user.id,name:user.userName},'HalaMadi')
-    return res.status(200).json({message:"Success",token})
+    const token = jwt.sign({ id: user.id, name: user.userName }, 'HalaMadi')
+    return res.status(200).json({ message: "Success", token })
 })
 export default router
