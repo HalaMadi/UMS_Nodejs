27a30bd7b1c9363f330c78b2b249e30a97b4c7fb
HalaMadi <Halamadi75@gmail.com>
2025-02-02 10:25:50 +0200

    Add auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,1 +1,17 @@
-  
+import jwt from 'jsonwebtoken'
+
+const auth = () => {
+    return (req, res, next) => {
+        try {
+            const { token } = req.headers;
+            const decode = jwt.verify(token, 'HalaMadi');
+            if (decode.role != 'admin') {
+                return res.status(400).json({ message: 'Not authorized' })
+            }
+            next();
+        } catch (error) {
+            return res.status(500).json({ message: 'Server Error', error })
+        }
+    }
+}
+export default auth
--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -1,26 +1,24 @@
 import { Router } from "express"
 import UserModel from "../../../DB/Model/user.model.js";
-import jwt from 'jsonwebtoken'
+import auth from "../../middleware/auth.js";
 
 const router = Router();
 
 // Get Users 
-router.get('/', async (req, res) => {
-    const users = await UserModel.findAll({
-        attributes: ['id','userName', 'email']
-    });
-    return res.status(200).json({ message: "Success", users })
+router.get('/', auth(), async (req, res) => {
+    try {
+        const users = await UserModel.findAll({
+            attributes: ['id', 'userName', 'email']
+        });
+        return res.status(200).json({ message: "Success", users })
+    } catch (error) {
+        return res.status(500).json({ message: 'Server Error', error })
+    }
 })
 // delete User 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth(), async (req, res) => {
     const { id } = req.params;
-    const {token}=req.headers;
- 
-    const decode=jwt.verify(token,'HalaMadi');
 
-    if(decode.role !='admin'){
-return res.status(400).json({message:'Not authorized'})
-    }
     const user = await UserModel.findByPk(id)
     if (!user) {
         return res.status(404).json({ message: 'User not found' })
